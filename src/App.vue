040682cd2838f5c885134ee57ae0d183b10a2a77
ymhierarch <ymhmsr@163.com>
2022-09-08 11:22:43 +0800

    添加并配置 element

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <el-button type="primary">Primary</el-button>
   </nav>
   <router-view/>
 </template>
